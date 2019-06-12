26db755dd429d0215f406766d94c609ef6478dcd
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2019-06-12 12:26:35 +0000

    fix indentation

--- charting_functions.R
+++ charting_functions.R
@@ -9,14 +9,14 @@ library(ggrepel)
 
 
 rename_and_filter_data <- function(chart_data, shortlist = NULL) {
-# This takes the raw data, and selects just the columns we'ere interested in.
-# it's easier to work with variable names that are in camel_case without spaces,
-# as you don't have to use backticks.
-# Follow the pattern below to add columns where necessary:
-# select(new_variable = `old variable`).
-# Some of the columns have odd formatting, like carriage returns (see the \r\n) below.
-# To more easily determine how R is determining the column names, read in the file without any formatting,
-# then run names(chart_data)
+  # This takes the raw data, and selects just the columns we'ere interested in.
+  # it's easier to work with variable names that are in camel_case without spaces,
+  # as you don't have to use backticks.
+  # Follow the pattern below to add columns where necessary:
+  # select(new_variable = `old variable`).
+  # Some of the columns have odd formatting, like carriage returns (see the \r\n) below.
+  # To more easily determine how R is determining the column names, read in the file without any formatting,
+  # then run names(chart_data)
   
   output <- chart_data %>%
     select(option_ref = `Option Reference Number\r\n(hide col before sharing)`,
@@ -53,67 +53,67 @@ reorder_categorical_variables <- function(chart_data) {
                                     "missing (Red RAG rating)" = "Missing evidence",
                                     "OK (Amber RAG rating)" = "OK evidence",
                                     "robust (green RAG rating)" = "Robust evidence"),
-    deliverability_risk = recode_factor(deliverability_risk, 
-                                        "0" = "No return",
-                                        .missing = "No return",
-                                        "Red RAG rating" = "Deliverability: Red",
-                                        "Amber RAG rating" = "Deliverability: Amber",
-                                        "Green RAG rating" = "Deliverability: Green"),
-    financial_impacts = recode_factor(financial_impacts,
-                                      "0" = "No return for financial impacts",
-                                      .missing = "No return for financial impacts",
-                                      "are missing" = "Financial impacts are missing",
-                                      "are indicative" = "Financial impacts are indicative",
-                                      "are robust" = "Financial impacts are robust"),
-    impact_on_outcomes = recode_factor(impact_on_outcomes,
-                           "0" = "No return",
-                           .missing = "No return",
-                           "missing" = "missing impact on outcomes",
-                           "low" = "low impact",
-                           "medium" = "medium impact",
-                           "high" = "high impact on outcomes"),
-    total_cost = recode_factor(total_cost,
-                               "0" = "No return",
-                               .missing = "No return",
-                               "More than £5.1m per year" = "More than £5.1m per year",
-                               "Between £1.1m and £5m per year" = "Between £1.1m and £5m per year",
-                               "Less than £1m per year / a net benefit" = "Less than £1m per year / a net benefit"),
-    strategic_alignment = recode_factor(strategic_alignment,
-                                        "0" = "No return",
-                                        .missing = "No return",
-                                        .default = "No return",
-                                        "1" = "1: low alignment",
-                                        "2" = "2",
-                                        "3" = "3",
-                                        "4" = "4",
-                                        "5" = "5: high alignment")
-    
+           deliverability_risk = recode_factor(deliverability_risk, 
+                                               "0" = "No return",
+                                               .missing = "No return",
+                                               "Red RAG rating" = "Deliverability: Red",
+                                               "Amber RAG rating" = "Deliverability: Amber",
+                                               "Green RAG rating" = "Deliverability: Green"),
+           financial_impacts = recode_factor(financial_impacts,
+                                             "0" = "No return for financial impacts",
+                                             .missing = "No return for financial impacts",
+                                             "are missing" = "Financial impacts are missing",
+                                             "are indicative" = "Financial impacts are indicative",
+                                             "are robust" = "Financial impacts are robust"),
+           impact_on_outcomes = recode_factor(impact_on_outcomes,
+                                              "0" = "No return",
+                                              .missing = "No return",
+                                              "missing" = "missing impact on outcomes",
+                                              "low" = "low impact",
+                                              "medium" = "medium impact",
+                                              "high" = "high impact on outcomes"),
+           total_cost = recode_factor(total_cost,
+                                      "0" = "No return",
+                                      .missing = "No return",
+                                      "More than £5.1m per year" = "More than £5.1m per year",
+                                      "Between £1.1m and £5m per year" = "Between £1.1m and £5m per year",
+                                      "Less than £1m per year / a net benefit" = "Less than £1m per year / a net benefit"),
+           strategic_alignment = recode_factor(strategic_alignment,
+                                               "0" = "No return",
+                                               .missing = "No return",
+                                               .default = "No return",
+                                               "1" = "1: low alignment",
+                                               "2" = "2",
+                                               "3" = "3",
+                                               "4" = "4",
+                                               "5" = "5: high alignment")
+           
     )
 }
 
 
 
 all_dimensions_chart <- function(chart_data) {
-sr_chart <- ggplot(data = chart_data, 
-                   mapping = aes(x = strategic_alignment,
-                                 y = evidence,
-                                 text = paste("Option ref:", option_ref))) +
-  geom_point(position = position_jitterdodge(jitter.width = 0.3,
-                                             jitter.height = 0.3,
-                                             dodge.width = 0.75),
-             shape = 21,
-             stroke = 0.3,
-             alpha = 0.5,
-             mapping = aes(fill = total_cost,
-                           size = financial_impacts)) +
-  facet_grid(forcats::fct_rev(deliverability_risk) ~ impact_on_outcomes) +
-  scale_fill_manual(values = c("black",
-                               "red",
-                               "orange",
-                               "green4")) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-
+  sr_chart <- ggplot(data = chart_data, 
+                     mapping = aes(x = strategic_alignment,
+                                   y = evidence,
+                                   text = paste("Option ref:", option_ref))) +
+    geom_point(position = position_jitterdodge(jitter.width = 0.3,
+                                               jitter.height = 0.3,
+                                               dodge.width = 0.75),
+               shape = 21,
+               stroke = 0.3,
+               alpha = 0.5,
+               mapping = aes(fill = total_cost,
+                             size = financial_impacts)) +
+    facet_grid(forcats::fct_rev(deliverability_risk) ~ impact_on_outcomes) +
+    scale_fill_manual(values = c("black",
+                                 "red",
+                                 "orange",
+                                 "green4")) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  
+  
 }
 
 
@@ -126,13 +126,13 @@ strategic_alignment_vs_impact <- function(chart_data) {
   
   
   sr_chart <- ggplot(data = chart_data, 
-         mapping = aes(x = strategic_alignment,
-                       y = impact_on_outcomes,
-                       text = paste("</br>Option ref:", option_ref,
-                                    "</br> Strategic alignment: ", strategic_alignment,
-                                    "</br> impact: ", impact_on_outcomes,
-                                    "</br> financial impact: ", financial_impacts,
-                                    "</br> total cost: " ,total_cost))) +
+                     mapping = aes(x = strategic_alignment,
+                                   y = impact_on_outcomes,
+                                   text = paste("</br>Option ref:", option_ref,
+                                                "</br> Strategic alignment: ", strategic_alignment,
+                                                "</br> impact: ", impact_on_outcomes,
+                                                "</br> financial impact: ", financial_impacts,
+                                                "</br> total cost: " ,total_cost))) +
     geom_point(position = position_jitterdodge(jitter.width = 0.3,
                                                jitter.height = 0.3,
                                                dodge.width = 0.75),
@@ -144,7 +144,7 @@ strategic_alignment_vs_impact <- function(chart_data) {
     scale_fill_manual(values = fills) +
     ggtitle("Strategic alignment vs impact on outcomes")
   
-
+  
 }
 
 evidence_vs_impact <- function(chart_data) {
@@ -173,7 +173,7 @@ evidence_vs_impact <- function(chart_data) {
                              size = financial_impacts)) +
     scale_fill_manual(values = fills) +
     ggtitle("Evidence vs impact on outcomes")
-
+  
 }
 
 strategic_alignment_vs_deliverability <- function(chart_data) {
